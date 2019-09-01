69a81d6f39bade0fcb11ff7c0f5b27a8e3e5a1c0
Luiz Henrique <luizhenrique98@gmail.com>
2019-09-01 09:34:33 -0300

    aaa

--- bot_redirecter.py
+++ bot_redirecter.py
@@ -1,4 +1,26 @@
 import requests
+import pyrebase
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+config = {
+  "apiKey": "AIzaSyDLWP-B57TLt9tBqHkz5oi-N0gtSBtJn7w",
+  "authDomain": "hackacampusparty.firebaseapp.com",
+  "databaseURL": "https://hackacampusparty.firebaseio.com/",
+  "storageBucket": "hackacampusparty.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+  content = request.get_json()
+  text = content.get('text')
+  chat_id = get_chat_id(content.get('name'))
+  send_message(text, chat_id)
+  return '', 204
 
 key = "841166799:AAG-HKf9gZKLLmaG6mp_4dgn4NNS2NIRRN8"
 
@@ -9,8 +31,10 @@ def send_message(message, chatid):
 
     r = requests.post(url = url_to_send, data = data)
 
-def main():
-    send_message("Eu gosto de batata", 890335959)
+def get_chat_id(name):
+    return db.child(name).child("chat_id").get().val()
+
+# def get_users_from_place(db, place):
 
 if __name__ == '__main__':
-    main()
+    app.run()
